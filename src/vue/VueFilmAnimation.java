585dd0494086a30289c203589dd598ee5eb62967
TMRomain <romainover51@gmail.com>
2020-12-02 18:17:54 -0500

    Ajout de l'affichage des message dans la vue Animation

--- src/vue/VueFilmAnimation.java
+++ src/vue/VueFilmAnimation.java
@@ -1,12 +1,22 @@
 package vue;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import com.sun.media.jfxmedia.logging.Logger;
 
 import controleur.ControleurFilmAnimation;
 import fonction.EnvoyerMessage;
+import fonction.RecupererMessage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import modele.Message;
 
 
 
@@ -24,7 +34,7 @@ public class VueFilmAnimation extends Vue {
 	public void activerControles() 
 	{	
 		super.activerControles();
-
+		LoadMessage();
 		Button BouttonEnvoyer = (Button)lookup("#BouttonEnvoyer");
 		BouttonEnvoyer.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -38,6 +48,41 @@ public class VueFilmAnimation extends Vue {
 	private void EnvoyerText() {
 		TextField text = (TextField)lookup("#Text");
 		EnvoyerMessage envoyerFonction = new EnvoyerMessage();
-		envoyerFonction.envoyer(text.getText(), "#filmsanimation");	
+		envoyerFonction.envoyer(text.getText(), "#filmsanimation");
+		LoadMessage();
 	}
+	
+	private void LoadMessage() {
+		RecupererMessage messageFonction = new RecupererMessage();
+		List<Message> listeMessages = new ArrayList<Message>();
+		listeMessages = messageFonction.RecupererMessage("C01F09YPR09");
+		Collections.reverse(listeMessages);
+		ScrollPane scroll = (ScrollPane)lookup("#scrollPane");
+		Pane messageBox = (Pane)lookup("#vBoxParents");
+		scroll.setContent(messageBox); 
+		for (Message message : listeMessages) 
+		{ 
+			Pane messagePane = new Pane();
+			Text textUtilisateur = new Text();
+			Text textMessage = new Text();
+			textUtilisateur.setText(message.user);
+			textMessage.setText(message.text);
+			textMessage.setLayoutX(90);
+			textMessage.setLayoutY(20);
+			textUtilisateur.setLayoutY(20);
+			messagePane.getChildren().add(textUtilisateur);
+			messagePane.getChildren().add(textMessage);
+			messagePane.setStyle("-fx-background-color: #5f7bd8");
+			messagePane.setMaxWidth(300);
+			messagePane.setMinHeight(50);
+			messageBox.getChildren().add(messagePane);
+			
+		}
+		scroll.setVvalue(1.0); 
+		scroll.setStyle("-fx-background: #232323; -fx-border-color: #232323;");
+		scroll.setHbarPolicy(ScrollBarPolicy.NEVER);
+	}
+	
+	
+	
 }
