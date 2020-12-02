92fde9540d98c378582d8fa2a0b2ac9516f9d289
TMRomain <romainover51@gmail.com>
2020-12-02 16:20:12 -0500

    Ajout de la recuperation de message dans la vue Film Action

--- src/vue/VueFilmAction.java
+++ src/vue/VueFilmAction.java
@@ -1,10 +1,14 @@
 package vue;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.sun.media.jfxmedia.logging.Logger;
 import fonction.RecupererMessage;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import modele.Message;
 import controleur.ControleurFilmAction;
 
@@ -28,12 +32,31 @@ public class VueFilmAction extends Vue {
 		RecupererMessage messageFonction = new RecupererMessage();
 		List<Message> listeMessages = new ArrayList<Message>();
 		listeMessages = messageFonction.RecupererMessage("C01E6U4MJ23");
-		Pane messageBox = (Pane)lookup("#messageBox");
+		Collections.reverse(listeMessages);
+		ScrollPane scroll = (ScrollPane)lookup("#scrollPane");
+		Pane messageBox = (Pane)lookup("#vBoxParents");
+		scroll.setContent(messageBox); 
 		for (Message message : listeMessages) 
 		{ 
+			Pane messagePane = new Pane();
+			Text textUtilisateur = new Text();
+			Text textMessage = new Text();
+			textUtilisateur.setText(message.user);
+			textMessage.setText(message.text);
+			textMessage.setLayoutX(90);
+			textMessage.setLayoutY(20);
+			textUtilisateur.setLayoutY(20);
+			messagePane.getChildren().add(textUtilisateur);
+			messagePane.getChildren().add(textMessage);
+			messagePane.setStyle("-fx-background-color: #5f7bd8");
+			messagePane.setMaxWidth(300);
+			messagePane.setMinHeight(50);
+			messageBox.getChildren().add(messagePane);
 			
 		}
-		
+		scroll.setVvalue(1.0); 
+		scroll.setStyle("-fx-background: #232323; -fx-border-color: #232323;");
+		scroll.setHbarPolicy(ScrollBarPolicy.NEVER);
 		
 		/*
 		Button actionCalculatrice = (Button) lookup("#action-calculatrice");
